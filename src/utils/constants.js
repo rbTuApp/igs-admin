b75d7118c1c1bd5b2f8e33f0070454ce7723e0bd
Roberto Borjas <rb@tuapplicacion.co>
2022-12-04 04:00:11 -0500

    s

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 const { default: axios } = require("axios");
-const URL = "https://www.server.369fm.com.co/";
+const URL = "https://server.369fm.com.co/";
 // const URL = "http://localhost:4242/";
 const consumeGet = async (url, params) => {
   return await axios.get(URL + url, {
